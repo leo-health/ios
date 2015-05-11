46dc521996acb6d148a901cacf559f444692ee63
zdrossman <github email address>
2015-05-11 09:21:44 -0400

    Add single view application blank project

---
+++ LeoTests/LeoTests.m
@@ -0,0 +1,40 @@
+//
+//  LeoTests.m
+//  LeoTests
+//
+//  Created by Zachary Drossman on 5/11/15.
+//  Copyright (c) 2015 Leo Health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface LeoTests : XCTestCase
+
+@end
+
+@implementation LeoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
