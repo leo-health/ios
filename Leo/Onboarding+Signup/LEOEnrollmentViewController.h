e902ae3c4d18e4c2857fa8f96a31e2832cf8aa7b
zdrossman <zach@leohealth.com>
2015-09-04 14:18:14 -0400

    317 - Add LEOEnrollmentViewController

---
+++ Leo/Onboarding+Signup/LEOEnrollmentViewController.h
@@ -0,0 +1,14 @@
+//
+//  LEOSignUpViewController.h
+//  Leo
+//
+//  Created by Zachary Drossman on 9/2/15.
+//  Copyright (c) 2015 Leo Health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LEOScrollableContainerView.h"
+
+@interface LEOEnrollmentViewController : UIViewController <LEOScrollableContainerViewDelegate, UITextFieldDelegate, UIScrollViewDelegate>
+
+@end
