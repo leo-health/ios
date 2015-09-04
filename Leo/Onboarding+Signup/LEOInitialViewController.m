d4f8841ab9bb536570ccbf2b1321bd5db7e2867d
zdrossman <zach@leohealth.com>
2015-09-04 12:16:22 -0400

    303 - Create LEOInitialViewController

---
+++ Leo/Onboarding+Signup/LEOInitialViewController.m
@@ -0,0 +1,22 @@
+//
+//  LEOInitialViewController.m
+//  Leo
+//
+//  Created by Zachary Drossman on 8/31/15.
+//  Copyright (c) 2015 Leo Health. All rights reserved.
+//
+
+#import "LEOInitialViewController.h"
+#import "UIImage+Extensions.h"
+
+@implementation LEOInitialViewController
+
+-(void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+    
+    
+    self.navigationController.navigationBarHidden = YES;
+}
+@end
