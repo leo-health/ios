3d6c0300f3e8b3a070daeadade286d9b8842be76
zdrossman <zach@leohealth.com>
2015-09-30 13:59:19 -0400

    386 - Initial files for onboarding additional children

---
+++ Leo/Onboarding+Signup/LEOReviewAndAddChildView.h
@@ -0,0 +1,13 @@
+//
+//  LEOReviewAndAddChildView.h
+//  Leo
+//
+//  Created by Zachary Drossman on 9/30/15.
+//  Copyright (c) 2015 Leo Health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LEOReviewAndAddChildView : UIView
+
+@end
---
+++ Leo/Onboarding+Signup/LEOReviewAndAddChildView.m
@@ -0,0 +1,64 @@
+//
+//  LEOReviewAndAddChildView.m
+//  Leo
+//
+//  Created by Zachary Drossman on 9/30/15.
+//  Copyright (c) 2015 Leo Health. All rights reserved.
+//
+
+#import "LEOReviewAndAddChildView.h"
+
+@implementation LEOReviewAndAddChildView
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    
+    if (self) {
+        [self setupConstraints];
+    }
+    
+    return self;
+}
+
+-(instancetype)init {
+    
+    self = [super init];
+    
+    if (self) {
+        [self setupConstraints];
+    }
+    
+    return self;
+}
+
+
+#pragma mark - Autolayout
+
+- (void)setupConstraints {
+    
+    NSBundle *mainBundle = [NSBundle mainBundle];
+    NSArray *loadedViews = [mainBundle loadNibNamed:@"LEOReviewAndAddChildView" owner:self options:nil];
+    LEOReviewAndAddChildView *loadedSubview = [loadedViews firstObject];
+    
+    [self addSubview:loadedSubview];
+    
+    loadedSubview.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self addConstraint:[self pin:loadedSubview attribute:NSLayoutAttributeTop]];
+    [self addConstraint:[self pin:loadedSubview attribute:NSLayoutAttributeLeft]];
+    [self addConstraint:[self pin:loadedSubview attribute:NSLayoutAttributeBottom]];
+    [self addConstraint:[self pin:loadedSubview attribute:NSLayoutAttributeRight]];
+}
+
+- (NSLayoutConstraint *)pin:(id)item attribute:(NSLayoutAttribute)attribute {
+    return [NSLayoutConstraint constraintWithItem:self
+                                        attribute:attribute
+                                        relatedBy:NSLayoutRelationEqual
+                                           toItem:item
+                                        attribute:attribute
+                                       multiplier:1.0
+                                         constant:0.0];
+}
+
+@end
