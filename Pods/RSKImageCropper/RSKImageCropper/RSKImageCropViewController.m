d4cd8e2094b77117ede1435d6dcc3a187738ace7
zdrossman <zach@leohealth.com>
2016-01-19 12:01:10 -0500

    658 - Update: RSKImageCropViewController with public zoomable property

--- Pods/RSKImageCropper/RSKImageCropper/RSKImageCropViewController.m
+++ Pods/RSKImageCropper/RSKImageCropper/RSKImageCropViewController.m
@@ -89,7 +89,9 @@ static const CGFloat kLayoutImageScrollViewAnimationDuration = 0.25;
         _applyMaskToCroppedImage = NO;
         _rotationEnabled = NO;
         _cropMode = RSKImageCropModeCircle;
+        _zoomable = YES;
     }
+
     return self;
 }
 
@@ -271,6 +273,8 @@ static const CGFloat kLayoutImageScrollViewAnimationDuration = 0.25;
         _imageScrollView = [[RSKImageScrollView alloc] init];
         _imageScrollView.clipsToBounds = NO;
         _imageScrollView.aspectFill = self.avoidEmptySpaceAroundImage;
+        _imageScrollView.minimumZoomScale == self.zoomable ? 0.0 : 1.0;
+        _imageScrollView.maximumZoomScale == self.zoomable ? 10.0 : 1.0;
     }
     return _imageScrollView;
 }
