1abdb856ddb3cd058654e28f8414460d9662c5bc
afanslau <afanslau@gmail.com>
2016-02-09 11:58:02 -0500

    801 - Fix: message sender shows credentials if available

--- Leo/Data Model/Support.m
+++ Leo/Data Model/Support.m
@@ -17,6 +17,14 @@
     
     if (self) {
         _jobTitle = jsonResponse[APIParamUserJobTitle];
+
+        // handle the case where job title is not included in the API response. From a data modeling perspective, the credentials feild is overloaded here, but allow for it as a fail safe
+        if (!_jobTitle) {
+            NSArray *credentials = jsonResponse[APIParamUserCredentials];
+            if (credentials.count > 0) {
+                _jobTitle = credentials[0];
+            }
+        }
     }
     
     return self;
