75ea98500858c9173e0dcc910c8b02598bb41c45
afanslau <afanslau@gmail.com>
2016-02-29 16:42:23 -0500

    938 - Fix: notes included in appointments

--- Leo/Views and Controllers/Appointment Flow/LEOAppointmentView.m
+++ Leo/Views and Controllers/Appointment Flow/LEOAppointmentView.m
@@ -74,6 +74,7 @@ IB_DESIGNABLE
 - (void)setNotesTextView:(JVFloatLabeledTextView *)notesTextView {
 
     _notesTextView = notesTextView;
+    _notesTextView.text = self.note;
     _notesTextView.delegate = self;
     _notesTextView.scrollEnabled = NO;
     _notesTextView.placeholder = @"What would you like to cover?";
@@ -199,14 +200,8 @@ IB_DESIGNABLE
 - (void)setNote:(NSString *)note {
 
     _note = note;
-
-    if (note) {
-
-        self.notesTextView.text = note;
-    } else {
-
-        self.notesTextView.text = nil;
-    }
+    self.prepAppointment.note = note;
+    self.notesTextView.text = note;
 }
 
 #pragma mark - Drill Button Formatting
@@ -358,6 +353,7 @@ IB_DESIGNABLE
     self.patient = prepAppointment.patient;
     self.provider = prepAppointment.provider;
     self.appointmentType = prepAppointment.appointmentType;
+    self.note = prepAppointment.note;
 
     // FIXME: there is probably a better way to trigger side effects here to avoid this weird necessary ordering
     // self.date must be set last here because self setProvider will reset prepAppointment.provider, which will in turn set prepAppointment.date to nil
@@ -381,6 +377,11 @@ IB_DESIGNABLE
             shouldChangeTextInRange:range
                     replacementText:text
            toValidateCharacterLimit:kCharacterLimitAppointmentNotes];
+}
+
+- (void)textViewDidChange:(UITextView *)textView {
+
+    self.note = textView.text;
 }
 
 
