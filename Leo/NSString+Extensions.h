e791bc14d848b0c39ea52a2087c5d7ed0e719794
zdrossman <zach@leohealth.com>
2016-03-20 18:36:03 -0400

    1086 - Fix: Add percentile to record

---
+++ Leo/NSString+Extensions.h
@@ -0,0 +1,15 @@
+//
+//  NSString+Extensions.h
+//  Leo
+//
+//  Created by Zachary Drossman on 3/20/16.
+//  Copyright © 2016 Leo Health. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Extensions)
+
++ (NSString *)leo_numericSuffix:(NSUInteger)number;
+
+@end
---
+++ Leo/NSString+Extensions.m
@@ -0,0 +1,36 @@
+//
+//  NSString+Extensions.m
+//  Leo
+//
+//  Created by Zachary Drossman on 3/20/16.
+//  Copyright © 2016 Leo Health. All rights reserved.
+//
+
+#import "NSString+Extensions.h"
+
+@implementation NSString (Extensions)
+
+/**
+ *  Determines the appropriate suffix to add on to a number when used in a sentence.
+ *  Adapted from http://stackoverflow.com/a/4011232/1938725
+ *
+ *  @param number the numeric day of the month (out of 31, 30, 29, or 28)
+ *
+ *  @return the suffix associated with that number
+ */
++ (NSString *)leo_numericSuffix:(NSUInteger)number {
+
+    if (number >= 11 && number <= 13) {
+        return @"th";
+    }
+
+    switch (number % 10) {
+        case 1:  return @"st";
+        case 2:  return @"nd";
+        case 3:  return @"rd";
+        default: return @"th";
+    }
+}
+
+@end
+
