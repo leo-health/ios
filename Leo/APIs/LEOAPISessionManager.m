fc3f7ef574403507f42df289d27f6595b88d9a27
zdrossman <zach@leohealth.com>
2016-04-29 12:32:36 -0400

    1180 - Feature: Development build support for master runnable instance

--- Leo/APIs/LEOAPISessionManager.m
+++ Leo/APIs/LEOAPISessionManager.m
@@ -32,16 +32,22 @@
         securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeCertificate];
         securityPolicy.validatesDomainName = NO;
         securityPolicy.allowInvalidCertificates = YES;
-#endif
-
-#if defined(INTERNAL) || defined(RELEASE)
-        securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModePublicKey];
-#endif
 
         NSString *certificatePath = [[NSBundle mainBundle] pathForResource:[Configuration selfSignedCertificate] ofType:@"cer"];
         NSData *certificateData = [[NSData alloc] initWithContentsOfFile:certificatePath];
         securityPolicy.pinnedCertificates = @[certificateData];
         _sharedClient.securityPolicy = securityPolicy;
+#endif
+
+#if defined(INTERNAL) || defined(RELEASE)
+        securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModePublicKey];
+
+        NSString *certificatePath = [[NSBundle mainBundle] pathForResource:[Configuration selfSignedCertificate] ofType:@"cer"];
+        NSData *certificateData = [[NSData alloc] initWithContentsOfFile:certificatePath];
+        securityPolicy.pinnedCertificates = @[certificateData];
+        _sharedClient.securityPolicy = securityPolicy;
+#endif
+
     });
     
     return _sharedClient;
