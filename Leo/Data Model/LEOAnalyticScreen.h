ef14d744ca9b83addd753d4b7f869d9e8045dfd1
Annie Graham <abgraham@stanford.edu>
2016-06-27 15:42:03 -0400

    Adds LEOAnalyticScreen wrapper class [ch142]

---
+++ Leo/Data Model/LEOAnalyticScreen.h
@@ -0,0 +1,15 @@
+//
+//  LEOAnalyticScreen.h
+//  Leo
+//
+//  Created by Annie Graham on 6/27/16.
+//  Copyright © 2016 Leo Health. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LEOAnalyticScreen : NSObject
+
++ (void)tagScreen:(NSString *)screenName;
+
+@end
