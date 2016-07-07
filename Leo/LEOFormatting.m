0da54c6c61b23daf1b789cf0a39dd0199a9c2e66
Annie Graham <abgraham@stanford.edu>
2016-07-07 12:12:07 -0400

    Adds LEOFormatting class [ch335]

---
+++ Leo/LEOFormatting.m
@@ -0,0 +1,13 @@
+//
+//  LEOFormatting.m
+//  Leo
+//
+//  Created by Annie Graham on 7/7/16.
+//  Copyright © 2016 Leo Health. All rights reserved.
+//
+
+#import "LEOFormatting.h"
+
+@implementation LEOFormatting
+
+@end
