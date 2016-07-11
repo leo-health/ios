302f72563cd5d287693539c0d41b34b8c8ea3b62
Annie Graham <abgraham@stanford.edu>
2016-07-11 15:09:25 -0400

    Code review

--- Leo/UIColor+LeoColors.h
+++ Leo/UIColor+LeoColors.h
@@ -18,72 +18,85 @@
  *  @return <#return value description#>
  */
 + (UIColor *) leo_orangeRed;
+
 /**
  *  R:1.0/255.0 G:192.0/255.0 B:228.0/255.0
  *
  *  @return <#return value description#>
  */
 + (UIColor *) leo_blue;
+
 /**
  *  R:91.0/255.0 G:217.0/255.0 B:152.0/255.0
  *
  *  @return <#return value description#>
  */
 + (UIColor *) leo_green;
+
 /**
  *  R: 203.0/255.0 G:112.0/255.0 B:215.0/255.0
  *
  *  @return <#return value description#>
  */
+
 + (UIColor *) leo_purple;
+
 /**
  *  R:229.0/255.0 G:86.0/255.0 B:122.0/255.0
  *
  *  @return <#return value description#>
  */
 + (UIColor *) leo_pink;
+
 /**
  *  R:244.0/255.0 G:67.0/255.0 B:54.0/255.0
  *
  *  @return <#return value description#>
  */
 + (UIColor *) leo_redBadge;
+
 /**
  *  R:255/255.0 G:255/255.0 B:255/255.0
  *
  *  @return <#return value description#>
  */
 + (UIColor *) leo_white;
+
 /**
  *  R:228.0/255.0 G:245.0/255.0 B:252.0/255.0
  *
  *  @return <#return value description#>
  */
 + (UIColor *) leo_lightBlue;
+
 /**
  *  R:124.0/255.0 G:124.0/255.0 B:124.0/255.0
  *
  *  @return <#return value description#>
  */
 + (UIColor *)leo_gray124;
+
 /**
  *  R:74.0/255.0 G:74.0/255.0 B:74.0/255.0
  *
  *  @return <#return value description#>
  */
 + (UIColor *)leo_gray74;
+
 /**
  *  R:176.0/255.0 G:176.0/255.0 B:176.0/255.0
  *
  *  @return <#return value description#>
  */
 + (UIColor *)leo_gray176;
+
 /**
  *  R:185.0/255.0 G:185.0/255.0 B:185.0/255.0
  *
  *  @return <#return value description#>
  */
 + (UIColor *)leo_gray185;
+
 /**
  *  R:227.0/255.0 G:227.0/255.0 B:227.0/255.0
  *
