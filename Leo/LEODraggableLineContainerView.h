011e761c529dbe13688aa0d6c0d18cfabc36a202
Annie Graham <abgraham@stanford.edu>
2016-07-25 12:16:11 -0400

    Adds draggable line animation [branch ch503]

---
+++ Leo/LEODraggableLineContainerView.h
@@ -0,0 +1,23 @@
+//
+//  LEODraggableLineContainer.h
+//  Leo
+//
+//  Created by Annie Graham on 7/25/16.
+//  Copyright © 2016 Leo Health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <TelerikUI/TelerikUI.h>
+
+@class LEOStickyHeaderView;
+
+@interface LEODraggableLineContainerView : UIView
+
+@property(strong, nonatomic) NSMutableArray *centerXValuesOfPointsOnGraph;
+@property(strong, nonatomic) TKChart *chart;
+@property(strong, nonatomic)NSLayoutConstraint *lineXPositionConstraint;
+
+- (void)initContainer;
+
+
+@end
