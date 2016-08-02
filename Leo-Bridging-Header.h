45e2f9236896608ccdc5efe52049c0215202e70c
afanslau <afanslau@gmail.com>
2016-08-02 13:15:30 -0400

    [ch367] allows user to send referrals via email/text in settings. swift!

---
+++ Leo-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "LEOPrefixHeader.pch"
+#import "Leo-Prefix.pch"
+
+#import "LEOFormatting.h"
+#import "LEOStyleHelper.h"
+#import "LEOAlertHelper.h"
+#import "UIImage+Extensions.h"
+
